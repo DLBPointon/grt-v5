41808ea9662921db8c45aaddec1d10ffaebb2d72
Damon-Lee Pointon <damonlbp@hotmail.co.uk>
2021-06-18 11:43:24 +0000

    Minor Updates

--- updater_cron.sh
+++ updater_cron.sh
@@ -1,1 +1,1 @@
-/home/grit/miniconda3/envs/grt-env/bin/python /home/grit/grt-v5/scripts/jira_2_db_update.py > $(date +'%F:%R').log
+/home/grit/miniconda3/envs/grt-env/bin/python /home/grit/grt-v5/scripts/jira_2_db_update.py > logs/$(date +'%F:%R').log
