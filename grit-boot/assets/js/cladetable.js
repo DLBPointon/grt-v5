cb597698080b090d99ee066ca0fbe8f402cfbded
DLBPointon <damonlbp@hotmail.co.uk>
2021-06-22 16:06:01 +0100

    Updated data #14

--- grit-boot/assets/js/cladetable.js
+++ grit-boot/assets/js/cladetable.js
@@ -3,31 +3,43 @@ function cladetabler() {
     var one = document.getElementById('CladeSelector');
     prefix = one.options[one.selectedIndex].value
 
-    var url = 'http://172.27.21.37:3000/gritdata?order=family_name.asc&prefix_sl=in.('+ prefix +')&select=sample_id,family_name,manual_interventions,chromosome_assignments'
+    var url = 'http://172.27.21.37:3000/gritdata?order=family_name.asc&prefix_sl=in.('+ prefix +')' +
+        '&select=sample_id,family_name,manual_interventions,chromosome_assignments,chromosome_naming,' +
+        'expected_sex,observed_sex,curated_allosomes,curated_autosomes'
 
     d3.json(url, function (error, data) {
         if (error) return console.warn(error);
         var one = [];
         var two = [];
         var three = [];
-        var four = [];
+        var five = [];
+        var six = [];
+        var seven = [];
+        var eight = [];
+        var nine = [];
 
         data.forEach((item) => {
             if (error) return console.warn(error);
             one.push(item['sample_id']);
-            two.push(item['family_name'])
-            three.push(item['manual_interventions'])
-            four.push(item['chromosome_assignments'])
+            two.push(item['family_name']);
+            three.push(item['manual_interventions']);
+            five.push(item['chromosome_naming']);
+            six.push(item['expected_sex']);
+            seven.push(item['observed_sex']);
+            eight.push(item['curated_allosomes']);
+            nine.push(item['curated_autosomes'])
         });
 
-        var all_values = [one, two, three, four]
+        var all_values = [one, two, three, five, six, seven, eight, nine]
 
         var datas = [{
                 type: 'table',
                 header:
                     {
                         values: [["Sample ID"], ['Family'],
-                            ['Manual Interventions'], ['Chromosome Count Outcome']],
+                            ['Manual Interventions'], ['Curation Outcome'],
+                            ['Expected Sex'], ['Observed Sex'],
+                            ['Curated Allosomes'], ['Curated Autosomes']],
                         align: "center"
                     },
                 cells:
