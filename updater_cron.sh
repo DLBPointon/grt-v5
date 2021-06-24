674f62dab56fb54e6cc77ceb9beae4238fa96441
Damon-Lee Pointon <damonlbp@hotmail.co.uk>
2021-06-24 13:42:49 +0000

    updates to updater ready for dot env update

--- updater_cron.sh
+++ updater_cron.sh
@@ -1,1 +1,10 @@
-/home/grit/miniconda3/envs/grt-env/bin/python /home/grit/grt-v5/scripts/jira_2_db_update.py > logs/$(date +'%F:%R').log
+cd /home/grit/grt-v5/scripts/;
+rm /home/grit/grt-v5/grit-boot/output/*;
+/home/grit/miniconda3/envs/grt-env/bin/python /home/grit/grt-v5/scripts/jira_connect.py;
+# This needs to be updated to using dot loader
+# /home/grit/miniconda3/envs/grt-env/bin/python /home/grit/grt-v5/scripts/jira_2_db_update.py > logs/$(date +'%F:%R').log
+cd /home/grit/grt-v5/;
+docker-compose down --volumes;
+git pull;
+docker-compose up &
+
