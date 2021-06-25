3da8ba9e3bff5f76856fe497e61b640350c7c676
Damon-Lee Pointon <damonlbp@hotmail.co.uk>
2021-06-25 10:22:58 +0000

    Finalising automation

--- updater_cron.sh
+++ updater_cron.sh
@@ -1,10 +1,9 @@
 cd /home/grit/grt-v5/scripts/;
 rm /home/grit/grt-v5/grit-boot/output/*;
-/home/grit/miniconda3/envs/grt-env/bin/python /home/grit/grt-v5/scripts/jira_connect.py;
+/home/grit/miniconda3/envs/grt-env/bin/python3 /home/grit/grt-v5/scripts/jira_connect.py;
 # This needs to be updated to using dot loader
 # /home/grit/miniconda3/envs/grt-env/bin/python /home/grit/grt-v5/scripts/jira_2_db_update.py > logs/$(date +'%F:%R').log
 cd /home/grit/grt-v5/;
-docker-compose down --volumes;
+/usr/local/bin/docker-compose down --volumes;
 git pull;
-docker-compose up &
-
+/usr/local/bin/docker-compose up &
