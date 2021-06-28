d761f02b63809c63892248ffe74969ff25ff529c
Damon-Lee Pointon <damonlbp@hotmail.co.uk>
2021-06-28 09:14:45 +0000

    Minor updates to add sleep commands, this seems to have fixed an error in sorted file production

--- updater_cron.sh
+++ updater_cron.sh
@@ -1,9 +1,11 @@
 cd /home/grit/grt-v5/scripts/;
-rm /home/grit/grt-v5/grit-boot/output/*;
+rm /home/grit/grt-v5/grit-boot/output/jira_dump.tsv;
+rm /home/grit/grt-v5/grit-boot/output/jira_dump.tsv.sorted;
 /home/grit/miniconda3/envs/grt-env/bin/python3 /home/grit/grt-v5/scripts/jira_connect.py;
-# This needs to be updated to using dot loader
-# /home/grit/miniconda3/envs/grt-env/bin/python /home/grit/grt-v5/scripts/jira_2_db_update.py > logs/$(date +'%F:%R').log
+sleep 5;
 cd /home/grit/grt-v5/;
 /usr/local/bin/docker-compose down --volumes;
+sleep 5;
 git pull;
+sleep 5;
 /usr/local/bin/docker-compose up &
