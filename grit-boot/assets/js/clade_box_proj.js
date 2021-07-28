3fb019f3ab50da78752acabc15ad467e7377c5f6
DLBPointon <damonlbp@hotmail.co.uk>
2021-07-28 16:18:09 +0100

    new file:   grit-boot/assets/js/clade_box_proj.js 	new file:   grit-boot/assets/js/clade_main_projects.js 	new file:   grit-boot/project_insights.html
     Changes not staged for commit:
    	modified:   grit-boot/assets/js/clade_box_proj.js
    	modified:   grit-boot/assets/js/clade_main_projects.js
    	modified:   grit-boot/assets/js/tabulatortable_raw.js
    	modified:   grit-boot/clade_insights.html
    	modified:   grit-boot/datedash.html
    	modified:   grit-boot/db_scripts/10_db_init.sql
    	modified:   grit-boot/index.html
    	modified:   grit-boot/maingraphs.html
    	modified:   grit-boot/output/jira_dump.tsv
    	modified:   grit-boot/output/jira_dump.tsv.sorted
    	modified:   grit-boot/project_insights.html
    	modified:   grit-boot/table.html
    	modified:   scripts/jira_connect.py

---
+++ grit-boot/assets/js/clade_box_proj.js
@@ -0,0 +1,61 @@
+function clade_box() {
+    var one = document.getElementById('CladeSelector');
+    prefix = one.options[one.selectedIndex].value
+
+    var three = document.getElementById('CladeGraphSelector2Y');
+    three = three.options[three.selectedIndex].value
+
+    var four = document.getElementById('CladeGraphSelector2C');
+    four = four.options[four.selectedIndex].value
+
+    var url = 'http://172.27.21.37:3000/gritdata?order=family_name.asc&prefix_sl=in.('
+        + prefix + ')&select=family_name,prefix_dl,' + three
+
+    d3.json(url, function (error, data) {
+        if (error) return console.warn(error);
+        var y = [];
+        var c = [];
+
+
+        data.forEach((item) => {
+            if (three.includes('length_after')) {
+                y.push((item['manual_interventions']/item['length_after'])*1000000000)
+            } else {
+                y.push(item[three]);
+            }
+
+            c.push(item[four]);
+        });
+
+        var trace1 = {
+            type: 'box',
+            x: c,
+            y: y,
+            transforms: [{
+                type: 'groupby',
+                groups: c
+            }]
+        };
+
+        var datas = [trace1];
+
+        var elmntr1 = document.getElementById("cladebox").clientWidth - 30
+
+
+        var layout = {
+            width: elmntr1,
+            autosize: true,
+            margin: {
+                l: 50,
+                r: 50,
+                t: 0
+            },
+        };
+
+        var config = {responsive: true, displayModeBar: true}
+        Plotly.react('cladebox', datas, layout, config)
+    })
+
+}
+
+clade_box()
